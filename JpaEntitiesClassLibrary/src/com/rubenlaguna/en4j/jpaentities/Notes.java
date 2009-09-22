45921e4f933837241558d43fabcd153ea07550ff
Rubén Laguna <ruben.laguna@gmail.com>
2009-09-22 19:56:43 +0200

    no changes

--- JpaEntitiesClassLibrary/src/com/rubenlaguna/en4j/jpaentities/Notes.java
+++ JpaEntitiesClassLibrary/src/com/rubenlaguna/en4j/jpaentities/Notes.java
@@ -18,16 +18,129 @@
  * You should have received a copy of the GNU General Public License
  * along with en4j.  If not, see <http://www.gnu.org/licenses/>.
  */
-
 package com.rubenlaguna.en4j.jpaentities;
 
+import java.util.Date;
+import javax.persistence.Basic;
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 
 /**
  *
  * @author Ruben Laguna <ruben.laguna at gmail.com>
  */
 @Entity
+@Table(name = "NOTES")
 public class Notes {
 
+    @Id
+    @Column(name = "ID")
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+    @Basic(optional = false)
+    @Column(name = "TITLE", length = 255)
+    private String title;
+    @Basic(optional = false, fetch = FetchType.LAZY)
+    @Column(name = "CONTENT", length = 5242880)
+    private String content;
+    @Basic(optional = false)
+    @Column(name = "CREATED")
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date created;
+    @Basic(optional = false)
+    @Column(name = "UPDATED")
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date updated;
+    @Column(name = "SOURCEURL")
+    private String sourceurl;
+
+    public Notes() {
+    }
+
+    public Notes(Integer id, String title, String content, Date created, Date updated) {
+        this.id = id;
+        this.title = title;
+        this.content = content;
+        this.created = created;
+        this.updated = updated;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public Date getCreated() {
+        return created;
+    }
+
+    public void setCreated(Date created) {
+        this.created = created;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getSourceurl() {
+        return sourceurl;
+    }
+
+    public void setSourceurl(String sourceurl) {
+        this.sourceurl = sourceurl;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Date getUpdated() {
+        return updated;
+    }
+
+    public void setUpdated(Date updated) {
+        this.updated = updated;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Notes)) {
+            return false;
+        }
+        Notes other = (Notes) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "com.rubenlaguna.noteentitieslibrary.Notes[id=" + id + "]";
+    }
 }
