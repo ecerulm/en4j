94b65a3953a10e400cc5a4597007bf0e8001b580
Rubén Laguna <ruben.laguna@gmail.com>
2009-11-01 20:43:04 +0100

    Added xhtml rendering component. Added jaxb21 directoty location as a part of private.properties

--- NBPlatformApp/NoteContentViewModule/src/com/rubenlaguna/en4j/NoteContentViewModule/NoteContentViewTopComponent.java
+++ NBPlatformApp/NoteContentViewModule/src/com/rubenlaguna/en4j/NoteContentViewModule/NoteContentViewTopComponent.java
@@ -2,43 +2,58 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package com.rubenlaguna.en4j.NoteContentViewModule;
 
 import com.rubenlaguna.en4j.jpaentities.Notes;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.StringReader;
 import java.util.Collection;
+import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import org.openide.util.Exceptions;
 import org.openide.util.LookupEvent;
 import org.openide.util.NbBundle;
 import org.openide.windows.TopComponent;
 import org.openide.windows.WindowManager;
 //import org.openide.util.ImageUtilities;
 import org.netbeans.api.settings.ConvertAsProperties;
+import org.openide.awt.NotificationDisplayer;
 import org.openide.util.Lookup;
 import org.openide.util.LookupListener;
 import org.openide.util.Utilities;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xhtmlrenderer.simple.XHTMLPanel;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
 
 /**
  * Top component which displays something.
  */
-@ConvertAsProperties(
-    dtd="-//com.rubenlaguna.en4j.NoteContentViewModule//NoteContentView//EN",
-    autostore=false
-)
+@ConvertAsProperties(dtd = "-//com.rubenlaguna.en4j.NoteContentViewModule//NoteContentView//EN",
+autostore = false)
 public final class NoteContentViewTopComponent extends TopComponent implements LookupListener {
 
     private static NoteContentViewTopComponent instance;
     /** path to the icon used by the component and its open action */
 //    static final String ICON_PATH = "SET/PATH/TO/ICON/HERE";
-
     private static final String PREFERRED_ID = "NoteContentViewTopComponent";
-
     private Lookup.Result result = null;
+    private XHTMLPanel panel = null;
+
     public NoteContentViewTopComponent() {
         initComponents();
         setName(NbBundle.getMessage(NoteContentViewTopComponent.class, "CTL_NoteContentViewTopComponent"));
         setToolTipText(NbBundle.getMessage(NoteContentViewTopComponent.class, "HINT_NoteContentViewTopComponent"));
 //        setIcon(ImageUtilities.loadImage(ICON_PATH, true));
+        panel = new XHTMLPanel();
+        jScrollPane2.setViewportView(panel);
 
     }
 
@@ -50,15 +65,7 @@ public final class NoteContentViewTopComponent extends TopComponent implements L
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        jLabel1 = new javax.swing.JLabel();
-        jScrollPane1 = new javax.swing.JScrollPane();
-        jTextArea1 = new javax.swing.JTextArea();
-
-        org.openide.awt.Mnemonics.setLocalizedText(jLabel1, org.openide.util.NbBundle.getMessage(NoteContentViewTopComponent.class, "NoteContentViewTopComponent.jLabel1.text")); // NOI18N
-
-        jTextArea1.setColumns(20);
-        jTextArea1.setRows(5);
-        jScrollPane1.setViewportView(jTextArea1);
+        jScrollPane2 = new javax.swing.JScrollPane();
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
         this.setLayout(layout);
@@ -66,27 +73,21 @@ public final class NoteContentViewTopComponent extends TopComponent implements L
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 388, Short.MAX_VALUE)
-                    .addComponent(jLabel1))
+                .addComponent(jScrollPane2, javax.swing.GroupLayout.DEFAULT_SIZE, 388, Short.MAX_VALUE)
                 .addContainerGap())
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
                 .addContainerGap()
-                .addComponent(jLabel1)
-                .addGap(18, 18, 18)
-                .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 254, Short.MAX_VALUE)
+                .addComponent(jScrollPane2, javax.swing.GroupLayout.DEFAULT_SIZE, 381, Short.MAX_VALUE)
                 .addContainerGap())
         );
     }// </editor-fold>//GEN-END:initComponents
-
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JLabel jLabel1;
-    private javax.swing.JScrollPane jScrollPane1;
-    private javax.swing.JTextArea jTextArea1;
+    private javax.swing.JScrollPane jScrollPane2;
     // End of variables declaration//GEN-END:variables
+
     /**
      * Gets default instance. Do not use directly: reserved for *.settings files only,
      * i.e. deserialization routines; otherwise you could get a non-deserialized instance.
@@ -100,12 +101,67 @@ public final class NoteContentViewTopComponent extends TopComponent implements L
     }
 
     public void resultChanged(LookupEvent ev) {
-        Collection<? extends Notes> notes = result.allInstances();
-        if(!notes.isEmpty()) {
-            Notes n = notes.iterator().next();
-            //jLabel1.setText(n.getContent());
-            jTextArea1.setText(n.getContent());
+        try {
+            Collection<? extends Notes> notes = result.allInstances();
+            if (!notes.isEmpty()) {
+                Notes n = notes.iterator().next();
+                //jLabel1.setText(n.getContent());
+                //jTextArea1.setText(n.getContent());
+
+
+                // Create a builder factory
+                DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+                factory.setValidating(false);
+                //factory.setFeature("http://xml.org/sax/features/external-parameter-entities", false);
+                factory.setFeature("http://apache.org/xml/features/nonvalidating/load-external-dtd", false);
+
+                // Create the builder and parse the file
+                String content = n.getContent();
+                Document doc = factory.newDocumentBuilder().parse(new InputSource(new StringReader(content)));
+
+                //Document doc2 = factory.newDocumentBuilder().parse(getClass().getResourceAsStream("/com/rubenlaguna/en4j/NoteContentViewModule/xhtmlstricttemplate.xhtml"));
+
+                //NodeList list = doc.getElementsByTagName("en-note");
+                //Element element = (Element) list.item(0);
+                //Node dup = doc2.importNode(element, true);
+
+                //doc2.getElementsByTagName("body").item(0).appendChild(dup);
+
+                panel.setDocument(doc);
+
+
+
+            }
+        } catch (SAXException e) {
+            // A parsing error occurred; the xml input is not valid
+            Logger.getLogger(getName()).log(Level.SEVERE, "Excepton", e);
+        } catch (ParserConfigurationException e) {
+            Logger.getLogger(getName()).log(Level.SEVERE, "Excepton", e);
+        } catch (IOException e) {
+            Logger.getLogger(getName()).log(Level.SEVERE, "Excepton", e);
+
         }
+
+    }
+
+    // Parses an XML file and returns a DOM document.
+    // If validating is true, the contents is validated against the DTD
+    // specified in the file.
+    public static Document parseXmlFile(String filename, boolean validating) {
+        try {
+            // Create a builder factory
+            DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+            factory.setValidating(validating);
+
+            // Create the builder and parse the file
+            Document doc = factory.newDocumentBuilder().parse(new File(filename));
+            return doc;
+        } catch (SAXException e) {
+            // A parsing error occurred; the xml input is not valid
+            } catch (ParserConfigurationException e) {
+        } catch (IOException e) {
+        }
+        return null;
     }
 
     /**
