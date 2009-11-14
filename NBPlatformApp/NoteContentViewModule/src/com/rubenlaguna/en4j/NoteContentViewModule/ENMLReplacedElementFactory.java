f192f237c0fe898057aee521ea9c0a887d807579
Rubén Laguna <ruben.laguna@gmail.com>
2009-11-14 16:45:37 +0100

    Replaced the en-media with a JTextArea saying "Missing implementation of en-media"

--- NBPlatformApp/NoteContentViewModule/src/com/rubenlaguna/en4j/NoteContentViewModule/ENMLReplacedElementFactory.java
+++ NBPlatformApp/NoteContentViewModule/src/com/rubenlaguna/en4j/NoteContentViewModule/ENMLReplacedElementFactory.java
@@ -5,11 +5,15 @@
 package com.rubenlaguna.en4j.NoteContentViewModule;
 
 import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
 import org.w3c.dom.Element;
 import org.xhtmlrenderer.extend.ReplacedElement;
 import org.xhtmlrenderer.extend.ReplacedElementFactory;
@@ -19,6 +23,7 @@ import org.xhtmlrenderer.render.BlockBox;
 import org.xhtmlrenderer.simple.extend.FormSubmissionListener;
 import org.xhtmlrenderer.swing.EmptyReplacedElement;
 import org.xhtmlrenderer.swing.ImageReplacedElement;
+import org.xhtmlrenderer.swing.SwingReplacedElement;
 import org.xhtmlrenderer.swing.SwingReplacedElementFactory;
 import org.xhtmlrenderer.util.ImageUtil;
 
@@ -51,11 +56,11 @@ class ENMLReplacedElementFactory implements ReplacedElementFactory {
         ReplacedElement toReturn = null;
 
 
-        Logger.getLogger(ENMLReplacedElementFactory.class.getName()).log(Level.INFO, "Element:"+box.getElement().getNodeName());
-        Logger.getLogger(ENMLReplacedElementFactory.class.getName()).log(Level.INFO, "Element content:"+box.getElement().getNodeValue());
+        Logger.getLogger(ENMLReplacedElementFactory.class.getName()).log(Level.INFO, "Element:" + box.getElement().getNodeName());
+        Logger.getLogger(ENMLReplacedElementFactory.class.getName()).log(Level.INFO, "Element content:" + box.getElement().getNodeValue());
 
         if ("en-media".equals(box.getElement().getNodeName())) {
-            toReturn = brokenImage(100,100);
+            toReturn = brokenImage(context, 100, 100);
         }
 
         if (null == toReturn) {
@@ -65,25 +70,25 @@ class ENMLReplacedElementFactory implements ReplacedElementFactory {
         return toReturn;
     }
 
-    private ReplacedElement brokenImage(int cssWidth, int cssHeight) {
-        BufferedImage missingImage = null;
-        ReplacedElement mre;
-        try {
-            // TODO: we can come up with something better; not sure if we should use Alt text, how text should size, etc.
-            missingImage = ImageUtil.createCompatibleBufferedImage(cssWidth, cssHeight, BufferedImage.TYPE_INT_RGB);
-            Graphics2D g = missingImage.createGraphics();
-            g.setColor(Color.BLACK);
-            g.setBackground(Color.WHITE);
-            g.setFont(new Font("Serif", Font.PLAIN, 12));
-            g.drawString("Missing", 0, 12);
-            g.dispose();
-            mre = new ImageReplacedElement(missingImage, cssWidth, cssHeight);
-        } catch (Exception e) {
-            LOG.log(Level.SEVERE,"couldn't create a <missing> image",e);
-            mre = new EmptyReplacedElement(
-                    cssWidth < 0 ? 0 : cssWidth,
-                    cssHeight < 0 ? 0 : cssHeight);
-        }
-        return mre;
+    private ReplacedElement brokenImage(LayoutContext context, int cssWidth, int cssHeight) {
+
+        //TODO: add a real implementation that returns an image
+        ReplacedElement toReturn = null;
+
+        JTextArea cc = new JTextArea();
+        cc.setText("Missing implementation for en-media");
+        //cc.setPreferredSize(new Dimension(cssWidth, cssHeight));
+        cc.setSize(cc.getPreferredSize());
+
+        context.getCanvas().add(cc);
+
+        toReturn = new SwingReplacedElement(cc) {
+
+            public boolean isRequiresInteractivePaint() {
+                return false;
+            }
+        };
+
+        return toReturn;
     }
 }
