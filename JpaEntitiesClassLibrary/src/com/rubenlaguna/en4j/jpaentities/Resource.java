f166a7209a4bad4261746523ab714ef3834816b3
Rubén Laguna <ruben.laguna@gmail.com>
2010-02-12 17:27:20 +0100

    GPL3.0 in JpaEntitiesClassLibrary

--- JpaEntitiesClassLibrary/src/com/rubenlaguna/en4j/jpaentities/Resource.java
+++ JpaEntitiesClassLibrary/src/com/rubenlaguna/en4j/jpaentities/Resource.java
@@ -1,3 +1,19 @@
+/*
+ *  Copyright (C) 2010 Ruben Laguna <ruben.laguna@gmail.com>
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package com.rubenlaguna.en4j.jpaentities;
 
 import java.io.Serializable;
@@ -44,7 +60,7 @@ public class Resource implements Serializable {
         this.dataBs = data;
     }
 
-    public byte[] getData(){
+    public byte[] getData() {
         return this.dataBs;
     }
 
