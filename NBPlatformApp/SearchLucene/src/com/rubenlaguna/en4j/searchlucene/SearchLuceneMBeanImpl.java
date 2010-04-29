bd6be9c77cff66c3ec9ca754abb6bd75c58bff86
Rubén Laguna <ruben.laguna@gmail.com>
2010-04-29 21:38:19 +0200

    Add GPLv3 notice
    Closes #80.

--- NBPlatformApp/SearchLucene/src/com/rubenlaguna/en4j/searchlucene/SearchLuceneMBeanImpl.java
+++ NBPlatformApp/SearchLucene/src/com/rubenlaguna/en4j/searchlucene/SearchLuceneMBeanImpl.java
@@ -1,14 +1,23 @@
 /*
- * SearchLuceneMBeanImpl.java
+ *  Copyright (C) 2010 Ruben Laguna <ruben.laguna@gmail.com>
  *
- * Created on March 2, 2010, 7:48 PM
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 package com.rubenlaguna.en4j.searchlucene;
 
-import java.util.Queue;
 import java.util.concurrent.ThreadPoolExecutor;
 import org.apache.commons.math.stat.descriptive.SynchronizedDescriptiveStatistics;
-import org.apache.lucene.document.Document;
 
 /**
  * Class SearchLuceneMBeanImpl
--- NBPlatformApp/Syncronization/src/com/rubenlaguna/en4j/sync/SyncToolbarJPanel.java
+++ NBPlatformApp/Syncronization/src/com/rubenlaguna/en4j/sync/SyncToolbarJPanel.java
@@ -1,12 +1,18 @@
 /*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/*
- * SyncToolbarJPanel.java
+ *  Copyright (C) 2010 Ruben Laguna <ruben.laguna@gmail.com>
  *
- * Created on Feb 14, 2010, 3:39:05 PM
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 package com.rubenlaguna.en4j.sync;
 
